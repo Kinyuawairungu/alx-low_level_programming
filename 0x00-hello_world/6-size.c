f088edfb5506f2fc0e9010a41921452e1dee3404
kinyuawairungu <danielirungu73@gmail.com>
2023-02-16 18:32:07 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int age;
+	long int myage;
+	long long int _myage;
+	float salary;
+
+printf("size of a char: %ld byte(s)\n", sizeof(a));
+printf("size of a int: %ld byte(s)\n", sizeof(age));
+printf("size of a long int: %ld byte(s)\n", sizeof(myage));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(_myage));
+printf("Size of a float: %ld byte(s)\n", sizeof(salary));
+return(0);
+return (0);
+}
