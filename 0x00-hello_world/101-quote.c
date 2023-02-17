50982238d235a36503f3a846a44c437fb4eda7f6
kinyuawairungu <danielirungu73@gmail.com>
2023-02-16 18:50:15 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unist.h>
+/**
+ * main - Entry point
+ * main - prints the standard error sandwiched arounf the standard outpu/t
+ *
+ * Return 1
+ * Return: returns 1
+ */
+-
+int main(void)
+{
+	write("and that piece of art is useful\" -Dora Korpar,2015-10-19\n");
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
