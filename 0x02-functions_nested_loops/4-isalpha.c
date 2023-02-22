fa524f54df2b1d3c47986aed0d38f73caafa9d90
kinyuawairungu <danielirungu73@gmail.com>
2023-02-22 10:44:36 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#includei "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1" for alphabetic character or 0 for anything else
+ */
+
+int _isalphat(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
