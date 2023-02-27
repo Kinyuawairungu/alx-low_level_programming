c1ace173af0429d1e8fcfa078e6f419de3c327bd
kinyuawairungu <danielirungu73@gmail.com>
2023-02-27 10:35:03 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	n = (longi / 2);
 
 	if ((longi % 2) == 1)
-		n = ((longo + 1) / 2);
+		n = ((longi + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
