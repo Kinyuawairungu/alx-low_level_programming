bfe94418901f280c4dcb98d9184166208d83f64b
kinyuawairungu <danielirungu73@gmail.com>
2023-03-11 11:47:03 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - print the number of arguments passed to the program
@@ -9,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
 (void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
