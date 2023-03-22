fada575b3df6dab96bf17c29e3954e5adda629d0
kinyuawairungu <danielirungu73@gmail.com>
2023-03-22 10:31:10 -0700

    0-print_name.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <unist.h>
 /**
- * main - Entry point
- * main - prints the standard error sandwiched arounf the standard outpu/t
  *
- * Return 1
+ * main - Entry point
+ *
+ *
  * Return: returns 1
  */
 -
 int main(void)
 {
-	write("and that piece of art is useful\" -Dora Korpar,2015-10-19\n");
 	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
