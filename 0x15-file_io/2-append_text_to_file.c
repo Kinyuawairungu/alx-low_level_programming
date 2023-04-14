37d38043bcad9c4605cabb53fc213ada67f6f4fd
kinyuawairungu <danielirungu73@gmail.com>
2023-04-14 12:40:51 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,27 +7,22 @@ include "main.h"
  * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
-
 {
 	int fd, len = 0, write_len;
 
 	if (!filename)
-
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-
 		return (-1);
 
 	if (text_content)
-
 	{
 		while (text_content[len])
 
 			len++;
-
 		write_len = write(fd, text_content, len);
 
 		if (write_len == -1)
@@ -38,6 +33,5 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	}
 	close(fd);
-
 	return (1);
 }
